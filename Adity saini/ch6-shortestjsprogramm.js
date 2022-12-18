f4790308d7b0b0cd99355df1418a51133ff33d76
ankit patidar <patiankit6555@gmail.com>
2022-12-18 12:03:46 +0530

    1js

--- Adity saini/ch6-shortestjsprogramm.js
+++
@@ -1,6 +0,0 @@
-// js create an window object even when you doesn't any thing inside your code
-// wheneven program run a global object is created a global execution context is created along with this this word is created
-
-// let talk more about windows
-// in case of browser global object is called windows it very depending on the where you are running javascript program
-// at global level in browser this is equal to window 
---
+++ Akshay saini/ch17-jsengine.js
@@ -0,0 +1,28 @@
+// javascript runtime environment is consist of :-
+// js engine
+// api
+// cq
+// mq and many more
+
+// but javascript env is not possible without js engine means it is heart of javascript env
+
+// js engine is not a machine
+
+// what happen inside js engine
+// code==> parsing ==> compilation ==> execution
+
+// code is broken down into tokens.
+// then syntext parser take the code and convert it into AST (anstract syntex tree)
+
+// after ast compilation stage come
+// js behave like the interpreter as well as compile language it mostly depend upon js engine
+// i.e, we call in JIT just in time compilation
+// it uses compiler as well as interpreter to run your code
+
+
+// our ast code is comnverted into byte code using interpretter and then this code move to execution it take help of compiler to optimise the code on the runtime
+
+// in some js engine aot ahead of time compilation 
+//and the above compilation is not possible without the help of memory head and call stack
+
+// read about mark and sweap algoritham
